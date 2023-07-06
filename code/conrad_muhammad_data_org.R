dc34fc7a194d10537e2f73a099c20bab841f179b
ashiqwisc <ashiq@wisc.edu>
2023-07-06 13:36:01 -0700

    Push for Conrad

--- code/conrad_muhammad_data_org.R
+++ code/conrad_muhammad_data_org.R
@@ -3,6 +3,13 @@
 df <- read.csv("./datasets/collapsed_AI_classroom_data.csv")
 
 # TODO: Convert screenalignment into binary column
+# Muhammad: This is my first attempt at doing this. Please let me know if this doesn't work well 
+
+df <- df %>%
+  mutate(
+    screenalignment_binary = ifelse(teacher_screenalignment >= 0.5, 1, 0)
+  )
+
 # TODO: Join learning gain data. "students.csv" matches the animal_id to anon "Stu" id. Learning gain data can be found at 
 # "meta-data-aied.csv" Note: 26 students missing conceptual knowledge pre, post and procedural knowledge pre. 32 students 
 # (including the 26) missing procedural knowledge post. This count can be found at "metadata_student_counts.csv"

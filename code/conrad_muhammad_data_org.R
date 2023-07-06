86766eae2cc70dc080b9e2626adcad60f81aae99
ashiqwisc <ashiq@wisc.edu>
2023-07-06 13:31:41 -0700

    Push for Conrad

--- code/conrad_muhammad_data_org.R
+++ code/conrad_muhammad_data_org.R
@@ -4,7 +4,7 @@ df <- read.csv("./datasets/collapsed_AI_classroom_data.csv")
 
 # TODO: Convert screenalignment into binary column
 # TODO: Join learning gain data. "students.csv" matches the animal_id to anon "Stu" id. Learning gain data can be found at 
-# "meta-data-aied.csv" Note: 26 students missing conceptual knowledge pre, post and prior knowledge pre. 32 students (including the 26)
-# missing prior knowledge post. This count can be found at "metadata_student_counts.csv"
+# "meta-data-aied.csv" Note: 26 students missing conceptual knowledge pre, post and procedural knowledge pre. 32 students 
+# (including the 26) missing procedural knowledge post. This count can be found at "metadata_student_counts.csv"
 # TODO: Calculate learning rates 
-# TODO: Split students by learning gain/learning rate into high vs. low
+# TODO: Split students by learning gain/learning rates into high vs. low
